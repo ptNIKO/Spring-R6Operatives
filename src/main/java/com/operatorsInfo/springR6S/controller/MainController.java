92f8628dda9cad150c67fc66d08a4e2de48ddbc3
ptNIKO <simonich2016@gmail.com>
2020-11-16 20:14:28 +0300

    add filter cards by name

--- src/main/java/com/operatorsInfo/springR6S/controller/MainController.java
+++ src/main/java/com/operatorsInfo/springR6S/controller/MainController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -20,6 +22,19 @@ public class MainController {
     public String main(Model model) {
         Iterable<Operative> operatives = operativeRepo.findAll();
         model.addAttribute("operatives", operatives);
+        return "mainPage";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam(value = "filter", required = false) String filter, Model model) {
+        Iterable<Operative> operatives;
+        if (filter != null && !filter.isEmpty()) {
+            operatives = operativeRepo.findByName(filter);
+        } else {
+            operatives = operativeRepo.findAll();
+        }
+        model.addAttribute("operatives", operatives);
+
         return "mainPage";
     }
 
--- src/main/java/com/operatorsInfo/springR6S/repos/OperativeRepo.java
+++ src/main/java/com/operatorsInfo/springR6S/repos/OperativeRepo.java
@@ -4,6 +4,10 @@ import com.operatorsInfo.springR6S.model.Operative;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OperativeRepo extends CrudRepository<Operative, Integer> {
+
+    List<Operative> findByName(String tag);
 }
